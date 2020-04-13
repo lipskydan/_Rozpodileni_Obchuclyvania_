f9155a7d13edaa244036c8e3fa4668e5401f3ffb
lipskydan <lipsky@knu.ua>
2020-04-13 10:12:18 +0300

    add Lab3 SecondPart

---
+++ SecondPart/Lab3/src/main/java/com/company/Main.java
@@ -0,0 +1,86 @@
+package com.company;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        Map map = new Map("MAP","localhost",3306,"root", "Steps.123");
+
+        System.out.println("-------------------------------------------");
+
+        map.showCountries();
+        map.showCities(1);
+        map.showCities(2);
+        map.showCities(3);
+
+        System.out.println("-------------------------------------------");
+
+        map.addCountry(1, "RASHA");
+        map.addCountry(2, "USA");
+        map.addCountry(3,"Canada");
+
+        map.showCountries();
+
+        System.out.println("-------------------------------------------");
+
+        map.addCity(1,1,"MOSCOW",11612943,1);
+        map.addCity(2,1,"SOCHI",343334,0);
+        map.addCity(3,2,"NEW YORK",8363710,0);
+        map.addCity(4,2,"LOS ANGELES",56451241,0);
+        map.addCity(5,3,"TORONTO",94234710,1);
+
+        map.showCities(1);
+        map.showCities(2);
+        map.showCities(3);
+        map.showCities(4);
+        map.showCities(5);
+
+        System.out.println("-------------------------------------------");
+
+        map.changeCountryInfo(1,"Russia");
+        map.showCountries();
+
+        map.changeCityInfo(5,3,"TORONTO",94234710,0);
+        map.showCities(3);
+
+        System.out.println("-------------------------------------------");
+
+        map.deleteCity(2);
+        map.deleteCity(1);
+
+        map.deleteCountry(1);
+
+        map.showCountries();
+        map.showCities(1);
+        map.showCities(2);
+        map.showCities(3);
+        map.showCities(4);
+        map.showCities(5);
+
+        System.out.println("-------------------------------------------");
+
+        map.findCity(3);
+        map.findCountry(2);
+
+        System.out.println("-------------------------------------------");
+
+
+        map.deleteCity(3);
+        map.deleteCity(4);
+        map.deleteCity(5);
+        map.deleteCountry(2);
+        map.deleteCountry(3);
+
+        map.showCountries();
+        map.showCities(1);
+        map.showCities(2);
+        map.showCities(3);
+        map.showCities(4);
+        map.showCities(5);
+
+        System.out.println("-------------------------------------------");
+
+    }
+}
