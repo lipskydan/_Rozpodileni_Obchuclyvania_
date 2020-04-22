4097f6cd8a8fc0a6ab2ad11bb541b03c93815984
lipskydan <lipsky@knu.ua>
2020-04-22 09:35:01 +0300

    add Lab9_5

---
+++ Lab9/Lab9_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab9_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test=new Test("./resources/index.html");
+            test.AddTask(100);
+            test.AddTask(500);
+            test.AddTask(1000);
+            test.AddTask(1500);
+            test.AddTask(2000);
+            test.AddTask(2500);
+            test.AddTask(3000);
+            test.Run();
+        }
+    }
+}
